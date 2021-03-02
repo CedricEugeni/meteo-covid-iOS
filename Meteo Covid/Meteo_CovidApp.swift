6296c10b95a022291773b87d33d60c3e131e889a
Cedric Eugeni <cedric.eugeni@gmail.com>
2021-03-02 13:46:19 +0100

    Created the xcode project

---
+++ Meteo Covid/Meteo_CovidApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meteo_CovidApp.swift
+//  Meteo Covid
+//
+//  Created by Cédric Eugeni on 02/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Meteo_CovidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
